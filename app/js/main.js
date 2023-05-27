8fff136c62674a7d3525e899a182030e6fbf79f7
merantauw <zholu.vlad2010@yandex.ru>
2023-05-27 22:58:07 +0500

    half-done scripts

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,40 @@
 $(function () {
-  
+
+  //плавный скролл
+  $(".menu__link, .header__logo, .footer__logo").on("click", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr('href'),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  });
+
+  $(window).scroll(function () {
+    $scrollPosition = $(window).scrollTop();
+    // console.log($scrollPosition); тестим
+    if ($scrollPosition > 50) {
+      $('.header__top').addClass('header__top--bg');
+    } else {
+      $('.header__top').removeClass('header__top--bg');
+    }
+  });
+
+  //материалы
+  $('.materials__item').hover( function(){
+    $('.materials__desc', this).toggleClass('active');
+  })
+
+  //секция вопросов
+  $('.faq__item-title').on('click', function () {
+    if ($('.faq__list').hasClass('one')) {
+      $('.faq__item-title').not($(this)).removeClass('active');
+      $('.faq__item-desc').not($(this).next()).slideUp(300);
+    }
+    $(this).toggleClass('active').next().slideToggle(300);
+  })
 })
--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ function styles() {
 
 function scripts() {
   return src([
-    // 'node_modules/jquery/dist/jquery.js',
+    'node_modules/jquery/dist/jquery.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
