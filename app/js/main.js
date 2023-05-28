6c520df62cd46efd8c0737af271ec3bca9a12439
merantauw <zholu.vlad2010@yandex.ru>
2023-05-28 19:17:45 +0500

    finished 70 adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -25,7 +25,7 @@ $(function () {
   });
 
   //материалы
-  $('.materials__item').hover( function(){
+  $('.materials__item').hover(function () {
     $('.materials__desc', this).toggleClass('active');
   })
 
@@ -37,4 +37,39 @@ $(function () {
     }
     $(this).toggleClass('active').next().slideToggle(300);
   })
+})
+
+const li = document.querySelectorAll('.menu__item');
+const sec = document.querySelectorAll('section');
+
+function activeMenu() {
+  let len = sec.length;
+  // console.log(sec);
+  while (--len && scrollY + 150 <= sec[len].offsetTop) {}
+  li.forEach(ltx => ltx.classList.remove('active'));
+  li[len].classList.add('active');
+}
+
+activeMenu();
+window.addEventListener('scroll', activeMenu);
+
+document.body.addEventListener('click', e => {
+  if (!e.target.matches('.slider__item')) return
+  document.querySelector('.slider__img-box img').src = e.target.dataset.src
+
+  document.querySelectorAll('.slider__item').forEach(btn => btn.classList.remove('active'))
+  e.target.classList.add('active')
+})
+
+//бургер
+$('.header__burger, .menu__item').on('click', function () {
+  $('.header__top').toggleClass('active');
+  $('.header__burger').toggleClass('active');
+  $('.body').toggleClass('lock');
+});
+
+$('.header__logo').on('click', function () {
+  $('.header__top').removeClass('active');
+  $('.header__burger').removeClass('active');
+  $('.body').removeClass('lock');
 })
